e99e07d9013901725049380730742328b8353a78
jqwez <jqwez@icloud.com>
2024-03-05 16:11:50 -0500

    createResource added to slide component

--- web/src/components/Slide.tsx
+++ web/src/components/Slide.tsx
@@ -1,4 +1,4 @@
-import { createSignal,  onMount } from 'solid-js'
+import { createResource, createSignal,  onMount } from 'solid-js'
 import './Slide.css'
 
 export type SlideData = {
@@ -15,26 +15,25 @@ export type SlideData = {
 type SlideProps = {
   SlideData: SlideData;
 }
-
+const getSlide = async (_url: string) => {
+    const resp = await fetch(_url,{
+      method: "GET",
+      redirect: "follow",})
+    const blob = await resp.blob()
+    return URL.createObjectURL(blob)
+    }
+  
 function Slide(props: SlideProps) {
   const url = `http://127.0.0.1:8000/blob/${props.SlideData.slide.url}`;
-  const [imageSrc, setImageSrc] = createSignal<string>("")
-  const getCat = async () => {
-    const req = await fetch(url,
-    {
-      method: "GET",
-      redirect: "follow",
-    });
-    const blob = await req.blob()
-    setImageSrc(URL.createObjectURL(blob))
-  }
-  onMount(()=> {
-    getCat();
-  });
+  const [imageSrc, setImageSrc] = createSignal<string>()
+  setImageSrc(url)
+  const [slide] = createResource(imageSrc, getSlide)
 
   return (
     <div class="_slide-div">
-    <img class="_slide" src={imageSrc()} height="400" width="600"/>
+      {slide.loading ? 
+     <div class="slide-placeholder">Loading...</div> :
+    <img class="_slide" src={slide()} height="400" width="600"/> }
     </div>
   )
 }
