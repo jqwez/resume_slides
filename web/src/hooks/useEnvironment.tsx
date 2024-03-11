8483512ce6cf91904ae22a17a713e46ea14bb706
Jeremy <jqwez@icloud.com>
2024-03-10 21:02:48 -0400

    env

--- web/src/hooks/useEnvironment.tsx
+++ web/src/hooks/useEnvironment.tsx
@@ -1,6 +1,6 @@
 
 
 export function useEnvironmentVariable(key: string, dev: string) {
-  const envVar = import.meta.env[key];
+  const envVar = import.meta.env[`VITE_${key}`];
   return envVar ? envVar : dev;
 }
